47ae143cbb1d14676fd45fb7fa392b75ef918236
shawn button <shawnbutton@gmail.com>
2016-02-20 15:55:05 -0500

    test for player purse

--- java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
@@ -11,10 +11,16 @@ public class PlayerTest {
     @Test
     public void player_should_have_name() {
         Player player = new Player("Bob");
-
         assertThat(player.getName(), is("Bob"));
-
     }
 
+    @Test
+    public void player_should_have_purse() {
+        Player player = new Player("Bob");
+        assertThat(player.getPurse(), is(0));
+
+        player.incrementPurse();
+        assertThat(player.getPurse(), is(1));
+    }
 
 }
