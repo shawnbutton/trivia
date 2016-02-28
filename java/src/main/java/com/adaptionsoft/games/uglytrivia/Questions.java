4a24f6524127e61e272617d200c1936bb3723ee8
shawn button <shawnbutton@gmail.com>
2016-02-27 21:29:46 -0500

    change == to .equals

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Questions.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Questions.java
@@ -35,13 +35,13 @@ public class Questions {
     public void askQuestions(int place) {
 
         String currentCategory = getCurrentCategory(place);
-        if (currentCategory == "Pop")
+        if (currentCategory.equals("Pop"))
             System.out.println(popQuestions.removeFirst());
-        if (currentCategory == "Science")
+        if (currentCategory.equals("Science"))
             System.out.println(scienceQuestions.removeFirst());
-        if (currentCategory == "Sports")
+        if (currentCategory.equals("Sports"))
             System.out.println(sportsQuestions.removeFirst());
-        if (currentCategory == "Rock")
+        if (currentCategory.equals("Rock"))
             System.out.println(rockQuestions.removeFirst());
     }
 
